8963ca5addbf1d9d6027185f45972acc3fdea890
ECUCONDOR <ysa@0378d.onmicrosoft.com>
2024-11-24 05:19:49 -0300

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,11 +3,11 @@ import { Pool } from 'pg'; // Usar pg para conectar con PostgreSQL
 
 // Crear una conexión a la base de datos
 const pool = new Pool({
-  user: 'eduardo',
-  host: 'localhost',
-  database: 'ecucondor_db',
-  password: 'ecucondor0812',
-  port: 5432,
+  user: process.env.DB_USER, // Usar variables de entorno
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
 });
 
 export async function POST(request: Request) {
@@ -16,7 +16,11 @@ export async function POST(request: Request) {
     
     // Extraer el archivo y los campos del formulario
     const file = formData.get('receipt') as File;
-    const userId = formData.get('user_id') as string;
+    const amountUSD = formData.get('amountUSD') as string;
+    const amountARS = formData.get('amountARS') as string;
+    const beneficiaryName = formData.get('beneficiaryName') as string;
+    const cbuOrAlias = formData.get('cbuOrAlias') as string;
+    const bank = formData.get('bank') as string;
 
     if (file) {
       // Convertir el archivo a un Buffer
@@ -30,11 +34,11 @@ export async function POST(request: Request) {
       const client = await pool.connect();
       try {
         const query = `
-          INSERT INTO receipts (user_id, file_data, file_name)
-          VALUES ($1, $2, $3)
+          INSERT INTO receipts (amount_usd, amount_ars, beneficiary_name, cbu_or_alias, bank, file_data, file_name)
+          VALUES ($1, $2, $3, $4, $5, $6, $7)
           RETURNING id
         `;
-        const values = [parseInt(userId), buffer, originalName];
+        const values = [parseFloat(amountUSD), parseFloat(amountARS), beneficiaryName, cbuOrAlias, bank, buffer, originalName];
         const result = await client.query(query, values);
 
         // Obtener el ID de la fila insertada para devolverlo como referencia
